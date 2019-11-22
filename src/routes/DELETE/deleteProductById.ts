2dba9a55c237b6bf9579fdf72c6284175eee5d13
Andrija <andrija@pointjupiter.com>
2019-11-22 13:37:27 +0100

    remove unused variable

--- src/routes/DELETE/deleteProductById.ts
+++ src/routes/DELETE/deleteProductById.ts
@@ -2,6 +2,6 @@ import { RequestHandler } from "express";
 import { client } from "../../app";
 
 export const deleteProduct: RequestHandler = async (req, res, next) => {
-  const { rows } = await client.query(`DELETE FROM products WHERE ${req.query.propname} = '${req.query.propvalue}'`)
+  await client.query(`DELETE FROM products WHERE ${req.query.propname} = '${req.query.propvalue}'`)
   res.send('success')
 }
